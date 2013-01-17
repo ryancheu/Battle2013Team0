577419cf82fc5698784960e8743c2d33be3262f5
zsherin <zarrro45@gmail.com>
2013-01-16 22:57:33 -0500

    added a robot that attacks on the 200th turn no matter what

---
+++ SimpleAttacking/Robots/ARobot.java
@@ -0,0 +1,31 @@
+package SimpleAttacking.Robots;
+
+import static SimpleAttacking.Util.Constants.TEAM_A_BROADCAST_OFFSET;
+import static SimpleAttacking.Util.Constants.TEAM_B_BROADCAST_OFFSET;
+
+import java.util.Random;
+
+
+
+import SimpleAttacking.Util.Radio;
+import battlecode.common.*;
+public class ARobot 
+{
+	public static RobotController mRC;
+	public static Radio mRadio;
+	public static Team mTeam;
+	public static Team mEnemy;
+	public static Random rand;
+	public ARobot(RobotController rc)
+	{
+		rand = new Random((int)(rc.getRobot().getID()*rc.getTeamPower()*Clock.getRoundNum()*Clock.getBytecodesLeft()));
+		mRC = rc;
+		mTeam = rc.getTeam();
+		mEnemy = mTeam.opponent();		
+		mRadio = new Radio(mRC, 
+				mTeam == Team.A ? TEAM_A_BROADCAST_OFFSET : TEAM_B_BROADCAST_OFFSET);
+	}
+	public void takeTurn() throws GameActionException{
+		
+	}
+}
