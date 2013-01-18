2ebcec6a6d3f084bc69ea9a73c16cccce7a60404
NicholasMohr <nmohr@mit.edu>
2013-01-17 21:23:43 -0500

    faster nuke

---
+++ pickaxeNuke/Robots/ARobot.java
@@ -0,0 +1,37 @@
+package pickaxeNuke.Robots;
+
+import static pickaxeNuke.Util.Constants.*;
+import static pickaxeNuke.Util.Util.*;
+
+import java.util.Random;
+
+import pickaxeNuke.Util.Radio;
+
+
+
+
+
+
+import battlecode.common.*;
+public class ARobot 
+{
+	public static RobotController mRC;
+	public static Radio mRadio;
+	public static Team mTeam;
+	public static Team mEnemy;
+	public static Random rand;
+	public ARobot(RobotController rc)
+	{
+		
+		rand = new Random((int)(rc.getRobot().getID()*rc.getTeamPower()*Clock.getRoundNum()*Clock.getBytecodesLeft()));
+		mRC = rc;
+		mTeam = rc.getTeam();
+		mEnemy = mTeam.opponent();		
+		mRadio = new Radio(mRC, 
+				mTeam == Team.A ? TEAM_A_BROADCAST_OFFSET : TEAM_B_BROADCAST_OFFSET);
+		setMapWidthAndHeight();
+	}
+	public void takeTurn() throws GameActionException{
+		
+	}
+}
