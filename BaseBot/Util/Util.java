4c30b3c0efd50331693f617bd97a7672009ef681
Ryan Cheu <ryancheu@mit.edu>
2013-01-19 20:37:40 -0500

    better micro for chokes

--- BaseBot/Util/Util.java
+++ BaseBot/Util/Util.java
@@ -420,54 +420,7 @@ public class Util {
 		return mineTeam != null && (mineTeam != mRC.getTeam());		
 	}
 	
-	
-	//returns the number of enemy/allied robots if a robot were to go in each direction.  
-	//number of allied is in 10s place, number of enemies is in 1s, a 100 means the direction is blocked
-	public static int[] getNeighborStats(int badLocs) throws GameActionException {
-
-		//TODO: Make this use a faster arraylist		
-
-		Robot[] NearbyRobots =  mRC.senseNearbyGameObjects(Robot.class, 2*2 + 2*2,ARobot.mEnemy); //2 in either direction
-
-		MapLocation roboLoc = mRC.getLocation();
-
-		//This array is NUM_DIR + 1 0s, the +1 is for the not moving location
-		int[] eachDirectionStats = { 0,0,0,0,0,0,0,0,0 }; 
-		ArrayList<LocationAndIndex> directionLocs = new ArrayList<LocationAndIndex>();
-		Direction tempDir;
-		MapLocation tempLoc;
-
-		//Initialize all the locations
-		for (int i = NUM_DIR; --i >= 0;) {
-			tempDir = DIRECTION_REVERSE[i];
-			tempLoc = roboLoc.add(tempDir);
-			if ( !isMineDirDanger(tempLoc) && mRC.canMove(tempDir) && ((badLocs >> (i)) & 1) != 1) {
-
-				directionLocs.add(new LocationAndIndex(roboLoc.add(tempDir),i));
-			}
-			else {
-				eachDirectionStats[i] = 100; //This signifies the spot is not movable
-			}
-		}
-
-		//Go through all the robots and see if they're near any of the squares next to us
-		MapLocation tempLocation = null;
-		int nearbyRobotsLength = NearbyRobots.length;
-		int directionLocsLength = directionLocs.size();
-		int j;
-		for ( int i = nearbyRobotsLength; --i >=0;) {
-			tempLocation = mRC.senseRobotInfo(NearbyRobots[i]).location;
-			for ( j = directionLocsLength; --j >= 0; ) {
-				if ( tempLocation.distanceSquaredTo(directionLocs.get(j).mp) <= 2 ) { // 2 means directly next to us					
-					eachDirectionStats[directionLocs.get(j).i] += 1;
-				}
-			}
-			if ( tempLocation.distanceSquaredTo(roboLoc) <= 2 ) {
-				eachDirectionStats[NUM_DIR] += 1;				
-			}
-		}
-		return eachDirectionStats;
-	}			
+			
 	
 	//Use these instead of just printing so we can disable easier	
 	public static void print(String text)
