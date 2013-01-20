827f6e6cef6958376585071a8b1fda281c6827a3
NicholasMohr <nmohr@mit.edu>
2013-01-19 22:19:39 -0500

    slight changes

--- BaseBot/Util/NukeConstants.java
+++ BaseBot/Util/NukeConstants.java
@@ -45,7 +45,7 @@ public class NukeConstants {
 	public static final int MAX_NUMBER_OF_ENCAMPMENTS = 15;
 	public static final int NUM_SOLDIERTYPES = 4;
 	public static final int NUM_OF_CENSUS_GENERATORTYPES = 1;
-	public static final double RATIO_OF_SUPPLIERS_OVER_GENERATORS = -1;
+	public static final double RATIO_OF_SUPPLIERS_OVER_GENERATORS = 0;
 	public static int Map_Width = 60;
 	public static int Map_Height = 60;
 
