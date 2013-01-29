c9e457dde2c33adefe39ba08eb6ba95059af8e03
NicholasMohr <nmohr@mit.edu>
2013-01-28 22:51:25 -0500

    changes to mines reverted

--- BaseBot/Robots/HQRobot.java
+++ BaseBot/Robots/HQRobot.java
@@ -150,7 +150,7 @@ public class HQRobot extends ARobot{
 		else{
 			//no team memory and it's a bad map for picknuke
 			
-			mType = HQType.RUSH;
+			mType = HQType.ECON;
 			mState = HQState.TURTLE;
 			
 		}
